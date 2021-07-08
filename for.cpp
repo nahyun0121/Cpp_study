9707c28facb61f2915c78e6c0247aaa6ec24ed35
Na <knh4769@khu.ac.kr>
2021-07-08 23:57:57 +0900

    2021-07-07 for loop

---
+++ for.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+/*
+int main() {
+	int N;
+	cin >> N;
+	if (N >= 1 && N <= 9) {
+		for (int i = 1; i <= 9; i++)
+			cout << N << " * " << i << " = " << N * i << endl;
+	}
+}
+*/
+
+/*
+int main() {
+	int n;
+	int sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+	cout << sum;
+
+	return 0;
+}
+*/
+
+/*
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+}
+*/
