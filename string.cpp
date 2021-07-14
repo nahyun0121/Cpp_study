cc229b5805a0c032e5803f9fffb83114edd95ced
Na <knh4769@khu.ac.kr>
2021-07-15 01:56:29 +0900

    2021-07-13 string

---
+++ string.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+void str(int R, string S) {
+	for (int j = 0; j < S.length(); j++) {
+		for (int k = 0; k < R; k++)
+			cout << S[j];
+	}
+}
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		int R;
+		cin >> R;
+		string S;
+		cin >> S;
+		str(R, S);
+		cout << endl;
+	}
+}
+*/
+
+
+/*
+int main() {
+	int N;
+	int sum = 0;
+	cin >> N;
+	if (N >= 1 && N <= 100) {
+		for (int i = 1; i <= N; i++) {
+			char num;
+			cin >> num;
+			int num1 = num - '0';
+			sum += num1;
+		}
+		cout << sum;
+	}
+}
+*/
+
+/*
+int main() {
+	char A;
+	cin >> A;
+	cout << (int)A << endl;
+}
+*/
