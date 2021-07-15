8ae6d7876ae9974f9b4ee12b07f0bc00b4922434
Nahyun Kim <knh4769@khu.ac.kr>
2021-07-15 17:05:05 +0900

    2021-07-15 math1(continue)

--- math1.cpp
+++ math1.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+/*
+int main() {
+	int X;
+	cin >> X;
+
+	int i = 1;
+	while (X > i) {
+		X -= i;
+		i++;
+	}
+
+	if (i % 2 == 1) {
+		cout << i - X + 1 << "/" << X;
+	}
+	else
+		cout << X << "/" << i - X + 1;
+
+	return 0;
+}
+*/
+
+/*
 int main() {
 	int N;
 	cin >> N;
@@ -11,18 +33,18 @@ int main() {
 		for (int sum = 2; sum <= N; i++)
 			sum += 6 * i;
 		cout << i;
-	}	
+	}
 
 	return 0;
 }
-
+*/
 
 /*
 int main() {
 	int A, B, C;
 	cin >> A >> B >> C;
 	int break_even_point = A / (C - B);
-	
+
 	if (C <= B) {
 		cout << -1;
 	}
